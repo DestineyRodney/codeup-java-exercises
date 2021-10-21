ae8dc9373eef07390d63ce10cae442c3f10605dd
Destiney Rodney <destiney.rodney01@gmail.com>
2021-10-21 16:14:08 -0500

    feat: java into exercise.

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello,");
+//        Refactor your code to use two System.out.print statements without changing the output.
+        System.out.print(" World!");
+
+    }
+}
+
+//\n creates a new line
+
+//    Try renaming the main method. What happens when you try to run the program?
+//Error: Main method not found in class HelloWorld, please define the main method as:
+//public static void main(String[] args)
+//        or a JavaFX application class must extend javafx.application.Application
